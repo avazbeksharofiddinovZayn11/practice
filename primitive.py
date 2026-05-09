c386771493eef1cc45d28c14b5b85e95ada52e0b
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-09 16:35:19 +0900

    feat: learn numbers in python

---
+++ primitive.py
@@ -0,0 +1,12 @@
+print("====== number ======")
+# in JAVA , variable is name storage location!
+#in Puthon, variable is named reference!
+
+count = 100
+count_type = type(count)
+print("count:", count, count_type)
+print(f"the count: {count} and type {count_type}")
+
+result1 = count.bit_count() # method
+result2 = count.numerator # state
+print(result1, result2)
