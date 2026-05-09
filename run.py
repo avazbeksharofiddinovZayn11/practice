e766a4d1921a50b46264f9b5381eaf57a5b5f843
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-09 16:26:57 +0900

    feat: Pythonsystem variables & special method via dunder

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
-massege = "Hello World!"
-print(massege)
+# Dunder __builtins__, __init__
 
-b = 43
-print(b)
+massage = "PYTHON: Everything is object"
+print(massage)
 
-a = 456
-print(a)
+result = type(massage)
+print("result:", result)
 
-insa = "salom"
-print(insa)
+''' In Python , there are builtin tools:
+(1) TYPES: int, float, str, list, dict
+(2) FUNCTIONS: print() len() input() type() str() int()
+CONSTANTS: True, False, None
+'''
+
+print(dir(__builtins__))
