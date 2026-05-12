457ea3c7b7d82b40ec955ccbc10109c4a8bfac39
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-12 14:39:15 +0900

    fix: handle all kind of Errors

--- objects.py
+++ objects.py
@@ -25,3 +25,25 @@ print("result1", result1)
 
 result2 = ceil(99.6)
 print("result2!", result2 )
+
+
+print("===== Error handling system =====")
+
+car_dict = dict(name="Tayota", year=2026, electric=True)
+
+try:
+    print("passed here")
+
+    a = car_dict.speed
+    result = car_dict["origin"]
+
+    print("result:", result)
+
+except Exception as err:
+    print("General Error:", err)
+
+else:
+    print("Executed successfully without errors")
+
+finally:
+    print("Final closing logic")
