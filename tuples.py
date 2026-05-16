e3e5e8cbadb808c7eb911ab7b12ee6fc5535b0fe
avazbeksharofiddinovZayn11 <avazbeksharofiddinov03@gmail.com>
2026-05-16 17:41:47 +0900

    feat: unpacking argument via tuple

--- tuples.py
+++ tuples.py
@@ -5,8 +5,8 @@
 '''
 
 print("=====  What is tuple: typle va list   =====")
-# java/PHP NodeJS array => Python list 
-# literal 
+# java/PHP NodeJS array => Python list
+# literal
 numbs = [2, 6, 34, 87]
 print(numbs)
 # constractor
@@ -24,3 +24,31 @@ tuple_obj = ("MIT", 100, True, None)
 
 print(animals[0])
 # animals[0] = "brid"
+
+# try avoid thse
+people = "Andrew", "John"
+animals = "dog",
+
+print("===== Unpacking arguments =====")
+groups = ["MIT", "FLEXY", "DEVEX", "MG"]
+(x, y, *z) = groups
+print(f"the x: {x} and y: {y}")
+print("z:", z)
+
+# *args > tuple
+
+
+def calculate(*args):
+    print("*args:", args)
+    total = 1
+    for x in args:
+        total *= x
+        # print(f"the type(args) value: {type(args)}")
+        print(f"the total value: {total}")
+        return total
+
+    # Call
+calculate(2, 5, 83, 93)
+calculate(0, 5, 567)
+print("------")
+calculate(7, 9)
